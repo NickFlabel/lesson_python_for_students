a9fa1c5cdcd1ad4c4993fd04e77d75fe41c35f22
Nick Selin <103569863+NickFlabel@users.noreply.github.com>
2024-10-07 18:54:06 +0500

    lesson 03.10

--- lesson8.py
+++ lesson8.py
@@ -31,5 +31,117 @@ next_next_middleware = AuthenticationMiddleware(next_middleware)
 # print(next_next_middleware(request))
 
 
+# Observer
+
+from abc import ABC, abstractmethod
+
+class Subject(ABC):
+    @abstractmethod
+    def register_observer(self, observer):
+        pass
+
+    @abstractmethod
+    def remove_observer(self, observer):
+        pass
+
+    @abstractmethod
+    def _notify_observers(self):
+        pass
 
 
+class Observer(ABC):
+    @abstractmethod
+    def update(self):
+        pass
+
+
+class WeatherData(Subject):
+    def __init__(self):
+        self.observers = []
+
+
+    def register_observer(self, observer):
+        self.observers.append(observer)
+
+    def remove_observer(self, observer):
+        self.observers.remove(observer)
+
+    def _notify_observers(self):
+        for observer in self.observers:
+            observer.update()
+
+    def event(self):
+        self._notify_observers()
+
+
+class CurrentConditionDisplay(Observer):
+    def __init__(self, subject: Subject):
+        self.weather_data = subject
+        self.weather_data.register_observer(self)
+
+    def update(self):
+        print('I was notified')
+
+
+weather_data = WeatherData()
+current_display = CurrentConditionDisplay(weather_data)
+weather_data.event()
+
+
+# Template (Шаблон)
+
+
+# Рецепт кофе - 1) вода 2) заварить кофе 3) перелить в чашку 4) добавить сахар и молоко
+# Рецепт чая - 1) вскипятить воду 2) заварить чай 3) перелить в чашку 4) добавить лимон
+
+
+
+class CaffeineBeverage(ABC):
+
+    def prepare_recipe(self):
+        self.boil_water()
+        self.prepare()
+        self.pour_in_cup()
+        self.add_extra_components()
+
+    @abstractmethod
+    def prepare(self):
+        pass
+
+    def add_extra_components(self):
+        pass
+
+    def boil_water(self):
+        print("Boiling water")
+
+    def pour_in_cup(self):
+        print("Pouring in a cup")
+
+
+class Tea(CaffeineBeverage):
+
+    def prepare(self):
+        print("preparing tea")
+
+
+tea = Tea()
+tea.prepare_recipe()
+
+# Шаблон определяет основные шаги алгоритма и позволяет подклассам предоставить реализацию
+# одного или нескольких шагов
+
+# Factory Method определяет интерфейс создания объекта, но позволяет подклассам 
+# выбирать конкретный класс создаваемого экземляра
+
+# Абстрактная фабрика определяет интерфейс для создания семейств взаимосвязанных объектов
+# без указания их конкретных классов
+
+# Фасад - структурный паттерн, котоырй предоставляет простой интерфейс к сложной системе 
+# классов, библиотеке или фреймворку
+
+# ORM - get(id)
+
+# Команда (Command) - поведенческий паттерн,, который превращает запросы в объекты, позволяя
+# передавать их как аргументы при вызове методов, поддерживает отмену операций
+
+# MVC - Model View Controller
